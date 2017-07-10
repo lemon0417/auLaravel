426fdadf05324de7f58007959fa9f9e6e038cbff
chrislin <lemon0417@gmail.com>
2017-07-10 15:12:11 +0800

    Use array and foreach

--- hotel/routes/web.php
+++ hotel/routes/web.php
@@ -15,12 +15,39 @@
 //     return view('welcome');
 // });
 
-Route::get('/', function(){
+Route::get('/', function() {
     return view('index', [
-        'name' => 'chrislin'
-        ]);
+        'rooms' => [
+            [
+                'title' => 'I SETTE CONI - TRULLO EDERA',
+                'price' => '2,143',
+                'type' => '整套房子／公寓',
+                'total' => '2'
+            ], [
+                'title' => 'I SETTE CONI - TRULLO EDERA',
+                'price' => '2,143',
+                'type' => '整套房子／公寓',
+                'total' => '2'
+            ], [
+                'title' => 'I SETTE CONI - TRULLO EDERA',
+                'price' => '2,143',
+                'type' => '整套房子／公寓',
+                'total' => '2'
+            ], [
+                'title' => 'I SETTE CONI - TRULLO EDERA',
+                'price' => '2,143',
+                'type' => '整套房子／公寓',
+                'total' => '2'
+            ], [
+                'title' => 'I SETTE CONI - TRULLO EDERA',
+                'price' => '2,143',
+                'type' => '整套房子／公寓',
+                'total' => '2'
+            ]
+        ]
+    ]);
 });
 
-Route::get('user/{id}', function($id){
+Route::get('user/{id}', function($id) {
     return 'Hello, '.$id;
 }) -> where('id', '[A-Za-z]+');
