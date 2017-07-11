5c364e667184b7fff70770b209605a06f5a175bd
chrislin <lemon0417@gmail.com>
2017-07-11 17:40:41 +0800

    add Index list

--- hotel/app/Http/Controllers/Guest/IndexController.php
+++ hotel/app/Http/Controllers/Guest/IndexController.php
@@ -4,39 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\RoomListModel;
 
 class IndexController extends Controller
 {
     public function show() {
+        $rooms = RoomListModel::whereColumn('total', '>', 'current')->get();
         return view('index', [
-            'rooms' => [
-                [
-                    'title' => 'I SETTE CONI - TRULLO EDERA',
-                    'price' => '2,143',
-                    'type' => '整套房子／公寓',
-                    'total' => '2'
-                ], [
-                    'title' => 'I SETTE CONI - TRULLO EDERA',
-                    'price' => '2,143',
-                    'type' => '整套房子／公寓',
-                    'total' => '3'
-                ], [
-                    'title' => 'I SETTE CONI - TRULLO EDERA',
-                    'price' => '2,143',
-                    'type' => '整套房子／公寓',
-                    'total' => '2'
-                ], [
-                    'title' => 'I SETTE CONI - TRULLO EDERA',
-                    'price' => '2,143',
-                    'type' => '整套房子／公寓',
-                    'total' => '2'
-                ], [
-                    'title' => 'I SETTE CONI - TRULLO EDERA',
-                    'price' => '2,143',
-                    'type' => '整套房子／公寓',
-                    'total' => '2'
-                ]
-            ]
+            'rooms' => $rooms
         ]);
     }
 }
